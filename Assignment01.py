5c7f5b5cc23515b99f1b5cf260e6c7e269c7122e
Jayeshnsharma <jayeshsharma2402@gmail.com>
2023-07-29 00:23:55 +0530

    Initial commit

---
+++ Assignment01.py
@@ -0,0 +1,4 @@
+print("First Program - Python Print Function \n"
+      "It is declared like this:\n"
+      "print('What to print')")
+print('Hello, "Jayesh"')
---
+++ input_function.py
@@ -0,0 +1,6 @@
+# name = input("What is your name?")
+# print(name)
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter Second number:"))
+print("num1 is:",num1,"And num2 is:",num2)
