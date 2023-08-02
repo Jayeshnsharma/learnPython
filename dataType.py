e5ed10214832224b33dc9bf116cb2512d3a5936d
Jayeshnsharma <jayeshsharma2402@gmail.com>
2023-08-02 18:11:50 +0530

    data type

--- dataType.py
+++ dataType.py
@@ -1,13 +1,21 @@
 print("Data Types")
-num1 = int(input("Enter num1:"))
-num2 = int(input("Enter num2:"))
-sum = num1 + num2
-sub = num1-num2
-mul = num1*num2
-div = num1/num2
-expo = num1**num2
-print("Sum of num1 and num2 is:", sum)
-print("Sub of num1 and num2 is:", sub)
-print("Mul of num1 and num2 is:", mul)
-print("Div of num1 and num2 is:", div)
-print("Expo of num1 and num2 is:", expo)
+# num1 = int(input("Enter num1:"))
+# num2 = int(input("Enter num2:"))
+# sum = num1 + num2
+# sub = num1-num2
+# mul = num1*num2
+# div = num1/num2
+# expo = num1**num2
+# print("Sum of num1 and num2 is:", sum)
+# print("Sub of num1 and num2 is:", sub)
+# print("Mul of num1 and num2 is:", mul)
+# print("Div of num1 and num2 is:", div)
+# print("Expo of num1 and num2 is:", expo)
+# print(type(sum))
+# print(type(sub))
+# print(type(mul))
+# print(type(div))
+# print(type(expo))
+
+name="Jayesh Sharma"
+print(name)
